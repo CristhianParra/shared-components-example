57ebfd1ce881ab127370d37cbc478920f5879d08
Cristhian Camilo Parra Moreno <cristhian.parra@avantica.net>
2020-08-26 14:54:13 -0500

    ok

---
+++ dist/AppetizeButton.d.ts
@@ -0,0 +1,6 @@
+/// <reference types="react" />
+declare type Props = {
+    label: string;
+};
+export declare function AppetizeButton(props: Props): JSX.Element;
+export {};
---
+++ dist/AppetizeButton.stories.d.ts
@@ -0,0 +1,7 @@
+import { AppetizeButton } from "./AppetizeButton";
+declare const _default: {
+    title: string;
+    component: typeof AppetizeButton;
+};
+export default _default;
+export declare const FirstStory: any;
